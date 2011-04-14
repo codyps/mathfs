c8c21c678165c2dcf26d83de6cd4462fdafe8833
Cody Schafer <cpschafer@gmail.com>
2011-04-13 21:00:25 -0400

    add empty readdir.

--- fs.c
+++ fs.c
@@ -31,7 +31,11 @@ static int m_getattr(const char *path, struct stat *stbuf)
 	return 0;
 }
 
-/* XXX: need readdir */
+static int m_readdir(const char *path, void *unk,
+		fuse_fill_dir_t fd, off_t off, struct fuse_file_info *fi)
+{
+	return 0;
+}
 
 static int m_open(const char *path, struct fuse_file_info *fi)
 {
@@ -47,8 +51,8 @@ static int m_read(const char *path, char *buf, size_t size,
 static struct fuse_operations math_fsops = {
 	.getattr	= m_getattr,
 	.open		= m_open,
-	.read		= m_read
-	/* XXX: need readdir */
+	.read		= m_read,
+	.readdir	= m_readdir
 };
 
 
