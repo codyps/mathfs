5ca308ed61ffff32b3fa4c69b9ec669001b76ca9
Cody Schafer <cpschafer@gmail.com>
2011-04-21 00:03:47 -0400

    fs: remove eval stuff.

--- fs.c
+++ fs.c
@@ -6,46 +6,6 @@
 #include <stdlib.h>
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
-int fibs(int opct, double *ops)
-{
-	return opct;
-}
-
-int add(int opct, double *ops)
-{
-	return opct;
-}
-
-int sub(int opct, double *ops)
-{
-	return opct;
-}
-
-struct math_op {
-	char *name;
-	int (*func)(int opct, double *ops);
-} math_ops [] = {
-	{ "add",  add },
-	{ "fibs", fibs },
-	{ "sub",  sub }
-};
-
-static int math_op_cmp_by_name(const void *p1, const void *p2)
-{
-	struct math_op const *m1 = p1, *m2 = p2;
-	return strcmp(m1->name, m2->name);
-}
-
-struct math_op *math_op_by_name(char *name)
-{
-	struct math_op tmp = {
-		.name = name
-	};
-
-	return bsearch(&tmp, math_ops, ARRAY_SIZE(math_ops),
-			sizeof(*math_ops), math_op_cmp_by_name);
-}
-
 static int m_getattr(const char *path, struct stat *stbuf)
 {
 	memset(stbuf, 0, sizeof(*stbuf));
@@ -57,10 +17,8 @@ static int m_getattr(const char *path, struct stat *stbuf)
 	return 0;
 }
 
-
-
 static int m_readdir(const char *path, void *unk,
-		fuse_fill_dir_t fd, off_t off, struct fuse_file_info *fi)
+		fuse_fill_dir_t filler, off_t off, struct fuse_file_info *fi)
 {
 
 	return 0;
@@ -76,6 +34,7 @@ static int m_read(const char *path, char *buf, size_t size,
 {
 	return size;
 }
+
 
 static struct fuse_operations math_fsops = {
 	.getattr	= m_getattr,
